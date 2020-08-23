a2dcc291fd89dbe55bf1a7adecadee907eced1a5
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-23 16:49:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,18 +1,9 @@
 array = [1,2,3]
 
 def square_array(array) 
-  
-  array.each { |i| arr << i ** 2 }
-
-  
-end
-puts new_numbers
-
-end
-
-
-def square_array(array)
   arr = []
   array.each { |i| arr << i ** 2 }
   arr
+  
 end
+
